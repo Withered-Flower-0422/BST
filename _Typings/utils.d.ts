37398f612dfadc9fa6e85dbf02dedaf9ca167942
withered_flower <2734850178@qq.com>
2025-05-28 22:26:46 +0800

    fix an edge case

--- _Typings/utils.d.ts
+++ _Typings/utils.d.ts
@@ -1,7 +1,9 @@
 declare module "utils" {
     type Equal<T, U> = (<G>() => G extends T ? 1 : 2) extends (<G>() => G extends U ? 1 : 2) ? true : false
 
-    type Mutable<T> = {
+    type _Mutable<T> = {
         [P in keyof T as Equal<{ [K in P]: T[K] }, { -readonly [K in P]: T[K] }> extends true ? P : never]: T[P]
     }
+
+    type Mutable<T> = Equal<_Mutable<T>, {}> extends true ? { [key: string | symbol]: never } : _Mutable<T>
 }
