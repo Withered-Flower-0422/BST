ceb069409e7b1f48bdd7ad80b566f538b1608323
withered_flower <2734850178@qq.com>
2025-05-30 21:00:17 +0800

    simplify export

--- _Typings/editorApi/editorApi.d.ts
+++ _Typings/editorApi/editorApi.d.ts
@@ -1,8 +1,5 @@
 declare module "editorApi" {
-    import { editor, console, file, scene, dialogWindowManager, math } from "editor:module"
+    export { editor, console, file, scene, dialogWindowManager, math } from "editor:module"
 
-    export {
-        editor, console, file, scene, dialogWindowManager, math,
-        Float2, Float3, Float4, Quaternion, Bool3, Trans, ColorRGBA
-    }
+    export { Float2, Float3, Float4, Quaternion, Bool3, Trans, ColorRGBA }
 }
--- _Typings/gameApi/gameApi.d.ts
+++ _Typings/gameApi/gameApi.d.ts
@@ -1,11 +1,8 @@
 declare module "gameApi" {
-    import {
+    export {
         math, console, scene, variables, settings, audioManager,
         levelManager, inputManager, uiCanvas, tweenManager,
     } from "game:module"
 
-    export {
-        math, console, scene, variables, settings, audioManager, levelManager, inputManager, uiCanvas, tweenManager,
-        Float2, Float3, Float4, Quaternion, Bool3, Trans, ColorRGBA
-    }
+    export { Float2, Float3, Float4, Quaternion, Bool3, Trans, ColorRGBA }
 }
