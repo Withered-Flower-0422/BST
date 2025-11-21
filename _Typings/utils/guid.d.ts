03c0c8d4aa024152f0cc21ce9eb10be3b8443fbe
withered_flower <2734850178@qq.com>
2025-11-22 02:02:32 +0800

    optimize `IsHexStringOfExactLength`

--- _Typings/utils/guid.d.ts
+++ _Typings/utils/guid.d.ts
@@ -2,7 +2,7 @@
 // For details: https://github.com/Withered-Flower-0422/BST/blob/main/LICENSE
 
 declare module "guid" {
-    type IsHexStringOfExactLength<
+    type _IsHexStringOfExactLength<
         S extends string,
         L extends number,
         B extends boolean = false,
@@ -14,8 +14,14 @@ declare module "guid" {
         : S extends `${
               | ("0" | "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9" | "a" | "b" | "c" | "d" | "e" | "f")
               | (B extends true ? "A" | "B" | "C" | "D" | "E" | "F" : never)}${infer R}`
-        ? IsHexStringOfExactLength<R, L, B, [...C, 0]>
+        ? _IsHexStringOfExactLength<R, L, B, [...C, 0]>
         : false
+
+    type IsHexStringOfExactLength<S extends string, L extends number, B extends boolean = false> = {
+        [I in L]: _IsHexStringOfExactLength<S, I, B>
+    }[L] extends false
+        ? false
+        : true
 
     type AssertGuid<S extends string> = string extends S
         ? S
