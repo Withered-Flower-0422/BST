aeee35ad52de785a0461858e7b49f2baa64ac885
Withered_Flower <2734850178@qq.com>
2025-12-16 18:30:26 +0800

    delete unused import statement

--- _Typings/gameApi/modules/scene.d.ts
+++ _Typings/gameApi/modules/scene.d.ts
@@ -2,7 +2,7 @@
 // For details: https://github.com/Withered-Flower-0422/BST/blob/main/LICENSE
 
 declare module "game:module" {
-    import { Item, Player, RaycastResult, CollisionLayer, BuiltinBallType, BallType } from "game:type"
+    import { Item, Player, RaycastResult, CollisionLayer } from "game:type"
     import { AssertGuid, AssertGuids } from "guid"
     import { AssertNonNeg } from "utils"
     import { Float3, Quaternion } from "basicData"
