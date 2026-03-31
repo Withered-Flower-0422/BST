5e50d196206b154ad04c7487385bc8b56c743aba
withered_flower <2734850178@qq.com>
2026-04-01 00:19:39 +0800

    enhance JSONExtension

--- _Typings/utils/JSONExtension.d.ts
+++ _Typings/utils/JSONExtension.d.ts
@@ -1,7 +1,7 @@
 // Licensed under the Apache License: http://www.apache.org/licenses/LICENSE-2.0
 // For details: https://github.com/Withered-Flower-0422/BST/blob/main/LICENSE
 
-import { AddTypeTo, TurnTypeTo, PickStringKey } from "utils"
+export {}
 
 declare const jsonDataType: unique symbol
 
@@ -12,18 +12,6 @@ declare global {
     interface JSON {
         parse<T>(text: json<T>): T
 
-        stringify<T>(
-            value: T extends bigint ? never : T,
-            replacer?: null,
-            space?: number,
-        ): json<
-            PickStringKey<
-                AddTypeTo<
-                    TurnTypeTo<T, undefined, Function | symbol>,
-                    null,
-                    number
-                >
-            >
-        >
+        stringify<T>(value: T, replacer?: null, space?: number): json<T>
     }
 }
